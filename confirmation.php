0be4a1343aa003cb43a0ca201f870b71125d5b32
mcabon26 <malo.cabon@gmail.com>
2023-11-13 17:36:39 +0100

    azert

--- confirmation.php
+++ confirmation.php
@@ -2,13 +2,21 @@
 <html>
 <head>
     <title>Confirmation d'Inscription</title>
+    <link rel="stylesheet" href="stylerphp.css">
+    <link href="https://fonts.googleapis.com/css?family=Faster+One&display=swap" rel="stylesheet" />
 </head>
 <body>
+
+
+<div class="divConf">
+
     <h1>Inscription Réussie</h1>
     <p>Merci, votre inscription a été effectuée avec succès !</p>
     <p>Vous êtes maintenant un membre enregistré.</p>
    
     <a href="inscription.php">Retour à l'Accueil</a>
+
+    </div>
 </body>
 </html>
 
