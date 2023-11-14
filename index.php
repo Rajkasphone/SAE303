bfd75ae43b44cb3879966e780b0a4678b6ec1b55
mcabon26 <malo.cabon@gmail.com>
2023-11-14 13:37:19 +0100

    azerty

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ma Page Web</title>
     <link rel="stylesheet" href="styles.css">
+    
 
     <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Faster+One&display=swap" rel="stylesheet" />
